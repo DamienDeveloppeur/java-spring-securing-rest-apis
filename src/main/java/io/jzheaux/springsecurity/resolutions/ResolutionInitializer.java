eae39196c269ab011cd2ae4e7c9c2e25f0faa2a0
merovingien <damienlhaurado@hotmail.com>
2023-05-01 15:21:15 +0200

    bloc 2 finish

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -3,14 +3,16 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
 	private final ResolutionRepository resolutions;
+	private final String PASSWORD = "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W";
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+	private final UserRepository users;
+
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository users) {
 		this.resolutions = resolutions;
+		this.users = users;
 	}
 
 	@Override
@@ -18,5 +20,21 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Read War and Peace", "user"));
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+		User user = new User("user", PASSWORD);
+		user.grantAuthority("resolution:read");
+		user.grantAuthority("resolution:write");
+		this.users.save(user);
+
+		User hasread = new User("hasread", PASSWORD);
+		user.grantAuthority("resolution:read");
+		this.users.save(hasread);
+
+		User haswrite = new User("haswrite", PASSWORD);
+		user.grantAuthority("resolution:write");
+		this.users.save(haswrite);
+
+
+
+
 	}
 }
